8dcab285311e78de769665a0295c15743679dec4
Henry <cataniahf@gmail.com>
2019-06-16 10:51:11 -0400

    Fixed petsorter, just some dumb mistakes

--- src/main/java/io/zipcoder/polymorphism/PetSorter.java
+++ src/main/java/io/zipcoder/polymorphism/PetSorter.java
@@ -7,19 +7,21 @@ public class PetSorter implements Comparator<Pets> {
 
     @Override
     public int compare(Pets o1, Pets o2) {
-            if (o2.type.toLowerCase().compareTo(o1.type.toLowerCase()) > 0) {
-                return 1;
-            } else if (o2.type.toLowerCase().compareTo(o1.type.toLowerCase()) < 1) {
+            if (o2.type.compareTo(o1.type) > 0) {
                 return -1;
+            } else if (o2.type.compareTo(o1.type) < 0) {
+                return 1;
             } else {
                 if (o2.name.compareTo(o1.name) > 0) {
-                    return 1;
-                } else if (o2.name.compareTo(o1.name) < 1) {
                     return -1;
+                } else if (o2.name.compareTo(o1.name) < 0) {
+                    return 1;
                 } else
+
                     return 0;
 
             }
+
         }
     }
 
